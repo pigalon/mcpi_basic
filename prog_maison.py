ab0a76a19d39a9cb57722bb800b2f4493d081430
pigalon <pigalon@gmail.com>
2020-04-20 00:48:31 +0200

    #1 build wall and air

--- prog_maison.py
+++ prog_maison.py
@@ -1,5 +1,5 @@
 from mcpi.minecraft import Minecraft
-import keyboard
+#import keyboard
 import mcpi.block as block
 
 ###########################################################
@@ -17,27 +17,42 @@ class Position:
 
     def afficher_position(self):
         print('coordonnees de la position x :' + str(self.x) +
-              '; y : ' + str(self.y) + '; z : ' + str(self.z))
+            '; y : ' + str(self.y) + '; z : ' + str(self.z))
 
 ##################################################################
 
 class Maison:
-    
     longueur = 0
     hauteur = 0
     profondeur = 0
-    
     position_debut = None
-    
+
     def __init__(self, longeur, hauteur, profondeur):
         self.longueur = longeur
         self.hauteur = hauteur
         self.profondeur = profondeur
-    
-    
-    def construire_maison(x, y, z):
-        # construire_mur
-        # construire_vide
+
+    def init_position_debut(self, x, y, z):
+        self.position_debut = Position(x+3, y, z+3)
+
+    def construire_murs(self):
+        fin = Position(self.position_debut.x+self.longueur, 
+                        self.position_debut.y+self.hauteur, self.position_debut.z+self.profondeur)
+        mc.setBlocks(self.position_debut.x, self.position_debut.y, 
+                        self.position_debut.z, fin.x, fin.y, fin.z, block.STONE.id)
+
+    def construire_vide(self):
+        debut = Position(self.position_debut.x+1, self.position_debut.y+1,
+                        self.position_debut.z+1)
+        fin = Position(debut.x+self.longueur-2, debut.y+self.hauteur-2,
+                        debut.z+self.profondeur-2)
+        mc.setBlocks(debut.x, debut.y, debut.z, fin.x,
+                        fin.y, fin.z, block.AIR.id)
+
+    def construire_maison(self, x, y, z):
+        self.init_position_debut(x, y, z)
+        self.construire_murs()
+        self.construire_vide()
         # construire_toit
         # construire_fenetres
         # construire_porte
@@ -46,34 +61,8 @@ class Maison:
         # mettre moquette
         print('construire maison')
 
+# Main
 
-    def construire_murs(x, y, z, longueur, hauteur, profondeur, type):
-        debut = Position(x+3, y, z+3)
-        debut.afficher_position()
-        fin = Position(debut.x+longueur, debut.y+hauteur, debut.z+profondeur)
-        mc.setBlocks(debut.x, debut.y, debut.z, fin.x, fin.y, fin.z, type)
-
-
-    def construire_vide(x, y, z, longueur, hauteur, profondeur):
-        debut = Position(x+3, y, z+3)
-        debut.afficher_position()
-        fin = Position(debut.x+longueur, debut.y+hauteur, debut.z+profondeur)
-        mc.setBlocks(debut.x, debut.y, debut.z, fin.x,
-                    fin.y, fin.z, block.EMPTY.id)
-
-
-# mettre dans une classe maison et associer les coordonnées à l amaison
-# nous ne sommes plus sur les coordonnées du joureur
-
-# def construire_vide(x, y, z, longueur, hauteur, profondeur, type):
-#     debut = Position(x+3, y, z+3)
-#     debut.afficher_position()
-#     fin = Position(debut.x+longueur, debut.y+hauteur, debut.z+profondeur)
-#     mc.setBlocks(debut.x, debut.y, debut.z, fin.x, fin.y, fin.z, type)
-
-
-# def construire_blocs_tnt(x, y, z):
-#     mc.setBlocks(x+1, y, z+1, x+11, y+11, z+11, 46, 2)
 LG_MAISON = 12
 HT_MAISON = 8
 PR_MAISON = 8
@@ -83,4 +72,5 @@ mc = Minecraft.create()
 
 x, y, z = mc.player.getTilePos()
 
-construire_murs(x=x, y=y, z=z, longueur=LG_MAISON, hauteur=HT_MAISON, profondeur=PR_MAISON, TYPE_BLOCK)
+maison = Maison(LG_MAISON, HT_MAISON, PR_MAISON)
+maison.construire_maison(x+3, y, z+3)
